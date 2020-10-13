f3d01cb27e7b40f7b5f777bd9b81356773e4afaa
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-13 12:27:54 +0530

    created schema for type of user and reset password

--- server/models/user.js
+++ server/models/user.js
@@ -1,36 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        trim: true,
-        required: true,
-        max: 12,
-        unique: true,
-        index: true,
-        lowercase: true
+      type: String,
+      trim: true,
+      required: true,
+      max: 12,
+      unique: true,
+      index: true,
+      lowercase: true,
     },
     name: {
-        type: String,
-        trim: true,
-        required: true,
-        max: 32
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
     },
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-        lowercase: true
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        trim: true,
-        required: true,
-        max: 32
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
     },
-    hashed_password:{
-        type: String,
-        required: true
-    }
-})
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    // this is for the strength of the password
+    salt: String,
+    role: {
+      type: String,
+      default: "subscriber",
+    },
+    resetPasswordLink: {
+      data: String,
+      default: "",
+    },
+  },
+//   this will give detail when user updated in database
+  { timestamps: true }
+);
