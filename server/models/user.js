e430ebd4dcecf29295e778d0fa5f559cb45ea83f
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-13 12:18:50 +0530

    define schema for email

--- server/models/user.js
+++ server/models/user.js
@@ -15,5 +15,13 @@ const userSchema = new mongoose.Schema({
         trim: true,
         required: true,
         max: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32,
+        unique: true,
+        lowercase: true
     }
 })
