0362eb42df08017f57e6f443f461b8e332509abd
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-13 11:23:04 +0530

    export the router module

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,3 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get('/api/register', (req, res) => {
+    res.json({
+        data: 'you hit register endpoint'
+    });
+});
+
+// export router so that we can use it on other files
+module.exports = router;
