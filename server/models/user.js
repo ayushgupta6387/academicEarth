16e708125660fe67c0e696fd4705e27e3dc6f893
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-13 12:17:11 +0530

    define user Schema for their name

--- server/models/user.js
+++ server/models/user.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 12,
+        unique: true,
+        index: true,
+        lowercase: true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32
+    }
+})
