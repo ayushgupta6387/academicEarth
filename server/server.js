533b40604089704cd820743ee02fe42b114921bb
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-14 10:59:57 +0530

    use the packages with the help of app.use method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-// cors is used to transfer data between one domain to another
+// cors is used to transfer data between one domain to another, our frontend will communicate with backend api
 // express validator: to give error when email is not valid or password
 // jsonwebtoken: to generate jsonwebtoken
 // morgan: whenever request coming from any route we can see in console
@@ -19,11 +19,18 @@ const app = express();
 // import routes
 const authRoutes = require('./routes/auth');
 
+// app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
 // get: receive the request that is coming from client side
 
 // middleware: code that you want to run in middle
 // whenever we have request we are forwarding it to authRoutes
 app.use('/api', authRoutes);
 
-const port = process.env.PORT || 8000
+
+// this will give port 8000 because we have written in env file
+const port = process.env.PORT;
 app.listen(port, () => console.log(`API is running on port ${port}`));
