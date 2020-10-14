93f5c91c0e5ca5ea231b3a87523fb85c52075a8b
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-14 12:06:36 +0530

    add some validators for name email and address

--- server/validators/auth.js
+++ server/validators/auth.js
@@ -1,1 +1,14 @@
 const { check } = require('express-validator');
+
+exports.userRegisterValidator = [
+    check('name')
+        .not()
+        .isEmpty()
+        .withMessage('Name is required'),
+    check('email')
+        .isEmail()
+        .withMessage('Enter valid email address'),
+    check('password')
+        .isLength({min: 6})
+        .withMessage('Password must be atleast 6 characters long')
+];
