dd3d6862e9f8e7030a3f3c513f5cfc243eadafe2
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-14 12:42:58 +0530

    pass the validations before register endpoint

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,8 +5,11 @@ const router = express.Router();
 // import from controller
 const {register} = require('../controllers/auth');
 
+// import vaidators
+const {userRegisterValidator} = require('../validators/auth');
+const {runValidation} = require('../validators');
 
 // apply validator before register controller
-router.post('/register', register);
+router.post('/register', userRegisterValidator, runValidation,register);
 // export router so that we can use it on other files
 module.exports = router;
