a36dc68bc5daaff582b0303a94eea10e2fa0fdfb
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-19 13:58:53 +0530

    define methods to send email

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,8 +8,43 @@ AWS.config.update({
 });
 
 // creating aws ses instance
-const ses = new AWS.ses({apiVersion: 2010-12-01});
+const ses = new AWS.SES({apiVersion: '2010-12-01'});
 
 exports.register = (req, res) => {
+    // getting data on server
   // console.log("REGISTER CONTROLLER", req.body);
+
+  const {name, email, password} = req.body;
+  const params = {
+    Source:process.env.EMAIL_FROM,
+    Destination: {
+      // this should be a array
+      ToAddresses: [email]
+    },
+    ReplyToAddresses: [process.env.EMAIL_TO],
+    Message:{
+      Body:{
+        Html:{
+          Charset: 'UTF-8',
+          Data: `<html><body><h1>Hello ${name}</h1 style="color:red;"><p>Test Email</p></body></html>`
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Complete Your Registration'
+      }
+    }
+  };
+  const sendEmailOnRegister = ses.sendEmail(params).promise();
+
+  sendEmailOnRegister
+  .then(data => {
+    console.log('email submitted to SES', data);
+    res.send('Email sent');
+  })
+
+  .catch(error => {
+    console.log('ses email on register', error);
+    res.send('email failed');
+  });
 };
