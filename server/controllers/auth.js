d95fc45bdfe61921a8b249bf4e0fa09943aa26cb
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-19 11:09:40 +0530

    setup some config for aws ses

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,14 @@
 
 const AWS = require('aws-sdk');
 
+AWS.config.update({
+  accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+  region:process.env.AWS_REGION
+});
+
+
+
 exports.register = (req, res) => {
   // console.log("REGISTER CONTROLLER", req.body);
 };
