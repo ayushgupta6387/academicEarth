c799b6b9475d57dee886ee5d93e972f5142a5af5
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-20 10:06:11 +0530

    take the token which is sent to user

---
+++ client/pages/auth/activate/[id].js
@@ -0,0 +1,9 @@
+
+// by writing this we get the link which is sent to user by email, on separate page when user click on that link
+import { withRouter } from 'next/router';
+
+const ActivateAccount = ({router}) => {
+    return <div>{JSON.stringify(router)}</div>
+};
+
+export default withRouter(ActivateAccount);
