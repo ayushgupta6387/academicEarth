47e51f710c17f9fa7d5aa8aea71e5fb40530b871
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-20 11:30:43 +0530

    created method for activating account on client side

--- client/pages/auth/activate/[id].js
+++ client/pages/auth/activate/[id].js
@@ -1,24 +1,69 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
-import { showErrorMessage, showSuccessMessage } from '../../../helpers/alerts';
+import { showSuccessMessage, showErrorMessage } from '../../../helpers/alerts';
 import { API } from '../../../config';
-import Layout from '../../../components/layout';
 
 
 // by writing this we get the link which is sent to user by email, on separate page when user click on that link
 import { withRouter } from 'next/router';
+import Layout from '../../../components/Layout';
 
-const ActivateAccount = ({router}) => {
-    const [state, setState]= useState({
+const ActivateAccount = ({ router }) => {
+    const [state, setState] = useState({
         name: '',
         token: '',
         buttonText: 'Activate Account',
         success: '',
         error: ''
+    });
+    // destructure so it becomes easier to use
+    const { name, token, buttonText, success, error } = state;
 
-    })
-    return <Layout>{JSON.stringify(router)}</Layout>
+    // we are using useEffect because we want to grab the token from the router and decode that so that username available
+
+    useEffect(() => {
+        // get token from user
+        let token = router.query.id;
+        if (token) {
+             // destructure the name and decode token
+            const { name } = jwt.decode(token);
+            setState({ ...state, name, token });
+        }
+        // [router]-->dependancy(prevent from infinite)
+    }, [router]);
+
+    const clickSubmit = async e => {
+        e.preventDefault();
+        // console.log('activate acccount');
+        // change text when button is clicked
+        setState({ ...state, buttonText: 'Activating' });
+
+        try {
+            const response = await axios.post(`${API}/register/activate`, { token });
+            // console.log('account activate response', response)
+            setState({ ...state, name: '', token: '', buttonText: 'Activated', success: response.data.message });
+        } catch (error) {
+            setState({ ...state, buttonText: 'Activate Account', error: error.response.data.error });
+        }
+    };
+
+    return (
+        <Layout>
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h1>G'day {name}, Ready to activate your account?</h1>
+                    <br />
+                    {/* show error msg when token expire */}
+                    {success && showSuccessMessage(success)}
+                    {error && showErrorMessage(error)}
+                    <button className="btn btn-outline-warning btn-block" onClick={clickSubmit}>
+                        {buttonText}
+                    </button>
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default withRouter(ActivateAccount);
