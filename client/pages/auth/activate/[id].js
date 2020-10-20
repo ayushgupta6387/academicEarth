a83d14aa2364370bece7100cce189cf4d2b29d1b
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-20 10:18:24 +0530

    import some other packages and use useState

--- client/pages/auth/activate/[id].js
+++ client/pages/auth/activate/[id].js
@@ -1,10 +1,22 @@
 import {useState, useEffect} from 'react'
 import jwt from 'jsonwebtoken';
+import axios from 'axios';
+import { showErrorMessage, showSuccessMessage } from '../../../helpers/alerts';
+import { API } from '../../../config';
+
 
 // by writing this we get the link which is sent to user by email, on separate page when user click on that link
 import { withRouter } from 'next/router';
 
 const ActivateAccount = ({router}) => {
+    const [state, setState]= useState({
+        name: '',
+        token: '',
+        buttonText: 'Activate Account',
+        success: '',
+        error: ''
+
+    })
     return <div>{JSON.stringify(router)}</div>
 };
 
