fa147cc92cf2cc3ad95f08472ff865ff286173ff
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-21 19:20:14 +0530

    apply middlewares

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,9 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+// import middlewares
+const {requireSignin, authMiddleware, adminMiddleware} = require('../controllers/auth');
+
 // import controllers
 // read user profile
 const {read} = require('../controllers/user');
 
+// routes
+// make request to /user, apply requireSignin so it is available if user logged in
+router.get('/user', requireSignin, authMiddleware, read);
 
 module.exports = router;
