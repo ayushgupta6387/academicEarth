db2cee7663dfade02def2c61f5f5ed973982b312
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-22 16:40:02 +0530

    created restriction method for user and admin

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -4,6 +4,6 @@ import { API } from '../../config';
 import { getCookie } from '../../helpers/auth';
 import withUser from '../withUser';
 
-const User = ({ user, token }) => <Layout>{JSON.stringify(user)}</Layout>;
+const User = ({ user }) => <Layout>{JSON.stringify(user)}</Layout>;
 
 export default withUser(User);
