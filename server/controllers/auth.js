5fe7417a3aebdb13ff54de0cc09950e2dd5c21c1
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-24 15:35:13 -0400

    reset password controller added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
 const { registerEmailParams,forgotPasswordEmailParams } = require('../helpers/email');
 const shortId = require('shortid');
+const _ = require('lodash')
 
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -190,5 +191,49 @@ exports.forgotPassword=(req,res)=>{
 }
 
 exports.resetPassword = (req,res)=>{
+    const {resetPasswordLink,newPassword}=req.body
+    if(resetPasswordLink){
+        //check for expiry
+
+        jwt.verify(resetPasswordLink,process.env.JWT_RESET_PASSWORD,(err,success)=>{
+            if(err){
+                return res.status(400).json({
+                    error:'Expired link try again'
+                })
+            }
+            //
+            User.findOne({resetPasswordLink}).exec((err,user)=>{
+                if(err || !user){
+                    return res.status(400).json({
+                        error:'invalid token try again'
+                    });
+                }
+
+
+                const updatedFields={
+                    password:newPassword,
+                    resetPasswordLink:''
+                }
+
+                user=_.extend(user,updatedFields)
+                user.save((err,result)=>{
+                    if(err){
+                        return res.status(400).json({
+                            error:'Password reset failed try again'
+                        });
+                    }
+                    res.json({
+                        message:'Great! now you can login with your new password'
+                    })
+                })
+
+    
+            })
+        })    
+
+
+
+        
+    }
 
 }
