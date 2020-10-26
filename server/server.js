2e01204b6dd672e97a40e8b46c59cbc44e4e16a6
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-26 03:22:48 -0400

    added category routes

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ mongoose
   .connect(process.env.DATABASE_CLOUD, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex:true
   })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
@@ -19,6 +20,7 @@ mongoose
 // import routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 // app middlewares
 app.use(morgan("dev"));
@@ -29,6 +31,7 @@ app.use(cors({ origin: process.env.CLIENT_URL }));
 // middlewares
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`API is running on port ${port}`));
