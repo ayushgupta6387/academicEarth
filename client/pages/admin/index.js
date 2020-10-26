c7310170e779a070dd670645aca46745f236eb22
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-26 12:54:46 -0400

    added option to create category

--- client/pages/admin/index.js
+++ client/pages/admin/index.js
@@ -1,6 +1,26 @@
 import Layout from '../../components/Layout';
 import withAdmin from '../withAdmin';
+import Link from 'next/Link'
+const Admin = ({ user }) => <Layout>
+    {/* {JSON.stringify(user)} */}
+    <h1>Admin Dashboard</h1>
+    <br/>
+    <div className='row'>
+        <div className='col-md-4'>
+            <ul className='nav flex-column'>
+                <li className='nav-item'>
+                        <Link href="/admin/category/create">
+                            <a className='nav-link'>Create Category</a>
+                        </Link>
+                </li>
+            </ul>
 
-const Admin = ({ user }) => <Layout>{JSON.stringify(user)}</Layout>;
+        </div>
+        <div className='col-md-8'></div>
+    </div>
+    
+    
+    
+    </Layout>;
 
 export default withAdmin(Admin);
