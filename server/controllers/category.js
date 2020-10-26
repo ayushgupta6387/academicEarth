96c1f6bd13d28365e72af69d5132f0970b5c7453
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-26 05:09:31 -0400

    controller to create category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,19 +1,32 @@
-exports.create = (req, res)={
-    
+const Category = require("../models/category");
+const slugify = require("slugify");
 
-}
+exports.create = (req, res) => {
+  const { name, content } = req.body;
+  const slug = slugify(name);
+  const image = {
+    url: "",
+    key: "123",
+  };
 
 
-exports.list = (req, res)={
 
-}
-exports.read = (req, res)={
+  const category = new Category({ name, slug, image });
+  category.postedBy=req.user._id;
+  category.save((err, data) => {
+    if (err) {
+      console.log("category create error", err);
+      return res.status(400).json({
+        error: " Category create failed",
+      });
+    }
+    res.json({
+      data,
+    });
+  });
+};
 
-}
-exports.update = (req, res)={
-
-}
-exports.remove = (req, res)={
-
-}
-
+exports.list = (req, res) = {};
+exports.read = (req, res) = {};
+exports.update = (req, res) = {};
+exports.remove = (req, res) = {};
