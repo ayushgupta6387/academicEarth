e69c5a0763d56ba83ecf18b51dd773a8b1267c3b
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-11-09 20:35:45 +0530

    created a created method for links

--- server/controllers/link.js
+++ server/controllers/link.js
@@ -3,8 +3,25 @@ const slugify = require('slugify');
 
 exports.create = (req, res) => {
     const { title, url, categories, type, medium } = req.body;
-    console.table({ title, url, categories, type, medium });
+    // console.table({ title, url, categories, type, medium });
+    const slug = url;
+    let link = new Link({ title, url, categories, type, medium, slug });
+    // posted by user
+    link.postedBy = req.user._id;
+    // categories
+    let arrayOfCategories = categories && categories.split(',');
+    link.categories = arrayOfCategories;
+    // save link
+    link.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Link already exist'
+            });
+        }
+        res.json(data);
+    });
 };
+
 
 exports.list = (req, res) => {
     //
