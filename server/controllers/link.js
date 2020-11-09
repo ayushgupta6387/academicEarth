40a839383d07157f7a8565e47ca832b5ff854237
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-09 12:27:51 +0530

    Update link.js

--- server/controllers/link.js
+++ server/controllers/link.js
@@ -13,69 +13,6 @@ AWS.config.update({
 
 const ses = new AWS.SES({ apiVersion: '2010-12-01' });
 
-exports.create = (req, res) => {
-    const { title, url, categories, type, medium } = req.body;
-    // console.table({ title, url, categories, type, medium });
-    const slug = url;
-    let link = new Link({ title, url, categories, type, medium, slug });
-    // posted by user
-    link.postedBy = req.user._id;
-    // save link
-    link.save((err, data) => {
-        if (err) {
-            return res.status(400).json({
-                error: 'Link already exist'
-            });
-        }
-        res.json(data);
-        // find all users in the category
-        User.find({ categories: { $in: categories } }).exec((err, users) => {
-            if (err) {
-                throw new Error(err);
-                console.log('Error finding users to send email on link publish');
-            }
-            Category.find({ _id: { $in: categories } }).exec((err, result) => {
-                data.categories = result;
-
-                for (let i = 0; i < users.length; i++) {
-                    const params = linkPublishedParams(users[i].email, data);
-                    const sendEmail = ses.sendEmail(params).promise();
-
-                    sendEmail
-                        .then(success => {
-                            console.log('email submitted to SES ', success);
-                            return;
-                        })
-                        .catch(failure => {
-                            console.log('error on email submitted to SES  ', failure);
-                            return;
-                        });
-                }
-            });
-        });
-    });
-};
-
-exports.list = (req, res) => {
-    let limit = req.body.limit ? parseInt(req.body.limit) : 10;
-    let skip = req.body.skip ? parseInt(req.body.skip) : 0;
-
-    Link.find({})
-        .populate('postedBy', 'name')
-        .populate('categories', 'name slug')
-        .sort({ createdAt: -1 })
-        .skip(skip)
-        .limit(limit)
-        .exec((err, data) => {
-            if (err) {
-                return res.status(400).json({
-                    error: 'Could not list links'
-                });
-            }
-            res.json(data);
-        });
-};
-
 exports.read = (req, res) => {
     const { id } = req.params;
     Link.findOne({ _id: id }).exec((err, data) => {
