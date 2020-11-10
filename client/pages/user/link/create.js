f1d58a66d7014acde3079ee3d7138e3f9bc8dacf
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-11-10 14:51:51 +0530

    created method for type and medium of link

--- client/pages/user/link/create.js
+++ client/pages/user/link/create.js
@@ -1,9 +1,9 @@
 // imports
-import { useState, useEffect } from 'react';
-import Layout from '../../../components/Layout';
-import axios from 'axios';
-import { API } from '../../../config';
-import { showSuccessMessage, showErrorMessage } from '../../../helpers/alerts';
+import { useState, useEffect } from "react";
+import Layout from "../../../components/Layout";
+import axios from "axios";
+import { API } from "../../../config";
+import { showSuccessMessage, showErrorMessage } from "../../../helpers/alerts";
 /*
 handle change title
 handle change url
@@ -20,112 +20,216 @@ get token of the logged in user - required to create link
  */
 
 const Create = () => {
-    // state
-    const [state, setState] = useState({
-        title: '',
-        url: '',
-        categories: [],
-        loadedCategories: [],
-        success: '',
-        error: '',
-        type: '',
-        medium: ''
-    });
+  // state
+  const [state, setState] = useState({
+    title: "",
+    url: "",
+    categories: [],
+    loadedCategories: [],
+    success: "",
+    error: "",
+    type: "",
+    medium: "",
+  });
 
-    const { title, url, categories, loadedCategories, success, error, type, medium } = state;
+  const {
+    title,
+    url,
+    categories,
+    loadedCategories,
+    success,
+    error,
+    type,
+    medium,
+  } = state;
 
-    // load categories when component mounts using useEffect
-    useEffect(() => {
-        loadCategories();
-    }, [success]);
+  // load categories when component mounts using useEffect
+  useEffect(() => {
+    loadCategories();
+  }, [success]);
 
-    const loadCategories = async () => {
-        const response = await axios.get(`${API}/categories`);
-        setState({ ...state, loadedCategories: response.data });
-    };
+  const loadCategories = async () => {
+    const response = await axios.get(`${API}/categories`);
+    setState({ ...state, loadedCategories: response.data });
+  };
 
-    const handleTitleChange = e => {
-        setState({ ...state, title: e.target.value, error: '', success: '' });
-    };
+  const handleTitleChange = (e) => {
+    setState({ ...state, title: e.target.value, error: "", success: "" });
+  };
 
-    const handleURLChange = e => {
-        setState({ ...state, url: e.target.value, error: '', success: '' });
-    };
+  const handleURLChange = (e) => {
+    setState({ ...state, url: e.target.value, error: "", success: "" });
+  };
 
-    const handleSubmit = async e => {
-        console.log('POST to server');
-    };
+  const handleSubmit = async (e) => {
+    console.log("POST to server");
+  };
 
-    const handleToggle = c => () => {
-        // return the first index or -1
-        const clickedCategory = categories.indexOf(c);
-        const all = [...categories];
+  const handleTypeClick = (e) => {
+    setState({ ...state, type: e.target.value, success: "", error: "" });
+  };
 
-        if (clickedCategory === -1) {
-            all.push(c);
-        } else {
-            all.splice(clickedCategory, 1);
-        }
-        console.log('all >> categories', all);
-        setState({ ...state, categories: all, success: '', error: '' });
-    };
+  const handleMediumClick = (e) => {
+    setState({ ...state, medium: e.target.value, success: "", error: "" });
+  };
 
-    // show categories > checkbox
-    // this function will return a list
-    const showCategories = () => {
-        return (
-            loadedCategories &&
-            loadedCategories.map((c, i) => (
-                <li className="list-unstyled" key={c._id}>
-                    <input type="checkbox" onChange={handleToggle(c._id)} className="mr-2" />
-                    <label className="form-check-label">{c.name}</label>
-                </li>
-            ))
-        );
-    };
+  const showMedium = () => (
+    <React.Fragment>
+      <div className="form-check ml-3">
+        <label className="form-check-label">
+          <input
+            type="radio"
+            onClick={handleMediumClick}
+            checked={medium === "video"}
+            value="video"
+            className="from-check-input"
+            name="medium"
+          />{" "}
+          Video
+        </label>
+      </div>
 
- // link create form
- const submitLinkForm = () => (
-    <form onSubmit={handleSubmit}>
-        <div className="form-group">
-            <label className="text">Title</label>
-            <input type="text" className="form-control" onChange={handleTitleChange} value={title} />
-        </div>
-        <div className="form-group">
-            <label className="text">URL</label>
-            <input type="url" className="form-control" onChange={handleURLChange} value={url} />
-        </div>
-        <div>
-            <button className="btn btn-outline-warning" type="submit">
-                Submit
-            </button>
-        </div>
-    </form>
-);
+      <div className="form-check ml-3">
+        <label className="form-check-label">
+          <input
+            type="radio"
+            onClick={handleMediumClick}
+            checked={medium === "book"}
+            value="book"
+            className="from-check-input"
+            name="medium"
+          />{" "}
+          Book
+        </label>
+      </div>
+    </React.Fragment>
+  );
 
+  const showTypes = () => (
+    <React.Fragment>
+      <div className="form-check ml-3">
+        <label className="form-check-label">
+          <input
+            type="radio"
+            onClick={handleTypeClick}
+            checked={type === "free"}
+            value="free"
+            className="from-check-input"
+            name="type"
+          />{" "}
+          Free
+        </label>
+      </div>
 
+      <div className="form-check ml-3">
+        <label className="form-check-label">
+          <input
+            type="radio"
+            onClick={handleTypeClick}
+            checked={type === "paid"}
+            value="paid"
+            className="from-check-input"
+            name="type"
+          />{" "}
+          Paid
+        </label>
+      </div>
+    </React.Fragment>
+  );
+
+  const handleToggle = (c) => () => {
+    // return the first index or -1
+    const clickedCategory = categories.indexOf(c);
+    const all = [...categories];
+
+    if (clickedCategory === -1) {
+      all.push(c);
+    } else {
+      all.splice(clickedCategory, 1);
+    }
+    console.log("all >> categories", all);
+    setState({ ...state, categories: all, success: "", error: "" });
+  };
+
+  // show categories > checkbox
+  // this function will return a list
+  const showCategories = () => {
     return (
-        <Layout>
-            <div className="row">
-                <div className="col-md-12">
-                    <h1>Submit Link/URL</h1>
-                    <br />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-4">
-                <div className="form-group">
-                        <label className="text ml-4">Category</label>
-                        <ul style={{ maxHeight: '100px', overflowY: 'scroll' }}>{showCategories()}</ul>
-                    </div>
-                </div>
-                
-                <div className="col-md-8">{submitLinkForm()}</div>
-            </div>
-            {JSON.stringify(categories)}
-         
-        </Layout>
+      loadedCategories &&
+      loadedCategories.map((c, i) => (
+        <li className="list-unstyled" key={c._id}>
+          <input
+            type="checkbox"
+            onChange={handleToggle(c._id)}
+            className="mr-2"
+          />
+          <label className="form-check-label">{c.name}</label>
+        </li>
+      ))
     );
+  };
+
+  // link create form
+  const submitLinkForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="text">Title</label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleTitleChange}
+          value={title}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text">URL</label>
+        <input
+          type="url"
+          className="form-control"
+          onChange={handleURLChange}
+          value={url}
+        />
+      </div>
+      <div>
+        <button className="btn btn-outline-warning" type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+
+  return (
+    <Layout>
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Submit Link/URL</h1>
+          <br />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="form-group">
+            <label className="text ml-4">Category</label>
+            <ul style={{ maxHeight: "100px", overflowY: "scroll" }}>
+              {showCategories()}
+            </ul>
+          </div>
+          <div className="form-group">
+            <label className="text ml-4">Type</label>
+            {showTypes()}
+          </div>
+          <div className="form-group">
+            <label className="text ml-4">Medium</label>
+            {showMedium()}
+          </div>
+        </div>
+
+        <div className="col-md-8">{submitLinkForm()}</div>
+      </div>
+      {JSON.stringify(type)}
+      {JSON.stringify(medium)}
+    </Layout>
+  );
 };
 
 export default Create;
