4952ff1cb8b5ec7103412ff54c4ec83fabc5def1
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-12 11:35:52 +0530

    Server Body Parser

--- server/server.js
+++ server/server.js
@@ -18,6 +18,11 @@ mongoose
     .then(() => console.log('DB connected'))
     .catch(err => console.log(err));
 
+// import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const linkRoutes = require('./routes/link');
 
 // app middlewares
 app.use(morgan('dev'));
@@ -26,3 +31,11 @@ app.use(bodyParser.json({ limit: '5mb', type: 'application/json' }));
 // app.use(cors());
 app.use(cors({ origin: process.env.CLIENT_URL }));
 
+// middlewares
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', linkRoutes);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`API is running on port ${port}`));
