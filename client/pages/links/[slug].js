b55d2742a0d02adfc7fc365bcc0a9e76c6620486
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-12 19:51:44 +0530

    Load Button Pop Up

--- client/pages/links/[slug].js
+++ client/pages/links/[slug].js
@@ -38,79 +38,30 @@ const Links = ({ query, category, links, totalLinks, linksLimit, linkSkip }) => 
                 </div>
             </div>
         ));
-        const loadMore = async () => {
-            let toSkip = skip + limit;
-            const response = await axios.post(`${API}/category/${query.slug}`, { skip: toSkip, limit });
-            setAllLinks([...allLinks, ...response.data.links]);
-            console.log('allLinks', allLinks);
-            console.log('response.data.links.length', response.data.links.length);
-            setSize(response.data.links.length);
-            setSkip(toSkip);
-        };
 
-        const loadMoreButton2 = () => {
-            return (
-                size > 0 &&
-                size >= limit && (
-                    <button onClick={loadMore} className="btn btn-outline-primary btn-lg">
-                        Load more
-                    </button>
-                )
-            );
-        };
+    const loadMore = async () => {
+        let toSkip = skip + limit;
+        const response = await axios.post(`${API}/category/${query.slug}`, { skip: toSkip, limit });
+        setAllLinks([...allLinks, ...response.data.links]);
+        console.log('allLinks', allLinks);
+        console.log('response.data.links.length', response.data.links.length);
+        setSize(response.data.links.length);
+        setSkip(toSkip);
+    };
+
+    const loadMoreButton = () => {
+        return (
+            size > 0 &&
+            size >= limit && (
+                <button onClick={loadMore} className="btn btn-outline-primary btn-lg">
+                    Load more
+                </button>
+            )
+        );
+    };
 
     return (
         <Layout>
-            <div className="row">
-                <div className="col-md-8">
-                <h1 className="display-4 font-weight-bold">{category.name} - URL/Links</h1>
-                    <div className="lead alert alert-secondary pt-4">{renderHTML(category.content || '')}</div>
-                </div>
-                <div className="col-md-4">
-                    <img src={category.image.url} alt={category.name} style={{ width: 'auto', maxHeight: '200px' }} />
-                </div>
-            </div>
-            allLinks.map((l, i) => (
-            <div className="row alert alert-primary p-2">
-                <div className="col-md-8">
-                    <a href={l.url} target="_blank">
-                        <h5 className="pt-2">{l.title}</h5>
-                        <h6 className="pt-2 text-danger" style={{ fontSize: '12px' }}>
-                            {l.url}
-                        </h6>
-                    </a>
-            <br />
-            <div className="row">
-                <div className="col-md-8">{listOfLinks()}</div>
-                <div className="col-md-4">
-                    <h2 className="lead">Most popular in {category.name}</h2>
-                    <p>show popular links</p>
-                </div>
-            </div>
-
-            <p>load more button</p>
-        </Layout>
-    );
-};
-
-Links.getInitialProps = async ({ query, req }) => {
-    let skip = 0;
-    let limit = 2;
-
-    const response = await axios.post(`${API}/category/${query.slug}`, { skip, limit });
-    return {
-        query,
-        category: response.data.category,
-        links: response.data.links,
-        totalLinks: response.data.links.length,
-        linksLimit: limit,
-        linkSkip: skip
-    };
-};
-
-export default Links;
-
-<Layout>
             <div className="row">
                 <div className="col-md-8">
                     <h1 className="display-4 font-weight-bold">{category.name} - URL/Links</h1>
@@ -131,3 +82,22 @@ export default Links;
 
             <div className="text-center pt-4 pb-5">{loadMoreButton()}</div>
         </Layout>
+    );
+};
+
+Links.getInitialProps = async ({ query, req }) => {
+    let skip = 0;
+    let limit = 2;
+
+    const response = await axios.post(`${API}/category/${query.slug}`, { skip, limit });
+    return {
+        query,
+        category: response.data.category,
+        links: response.data.links,
+        totalLinks: response.data.links.length,
+        linksLimit: limit,
+        linkSkip: skip
+    };
+};
+
+export default Links;
