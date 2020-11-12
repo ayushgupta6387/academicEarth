00a3eac0a5f9ec57eb943af3fdcfa3771bf1e821
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-12 21:12:04 +0530

    Button Rows

--- client/pages/links/[slug].js
+++ client/pages/links/[slug].js
@@ -85,13 +85,7 @@ const Links = ({ query, category, links, totalLinks, linksLimit, linkSkip }) => 
                 </div>
             </div>
             <br />
-            <div className="row">
-                <div className="col-md-8">{listOfLinks()}</div>
-                <div className="col-md-4">
-                    <h2 className="lead">Most popular in {category.name}</h2>
-                    <p>show popular links</p>
-                </div>
-            </div>
+            
 
             {/*<div className="text-center pt-4 pb-5">{loadMoreButton()}</div>*/}
 
