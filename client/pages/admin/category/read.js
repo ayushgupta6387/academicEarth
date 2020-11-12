bceb4731ea02fc22533f88035408a0565ca0e3db
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-12 16:28:40 +0530

    category link read

--- client/pages/admin/category/read.js
+++ client/pages/admin/category/read.js
@@ -18,3 +18,31 @@ const Read = ({ user, token }) => {
     useEffect(() => {
         loadCategories();
     }, []);
+
+    const loadCategories = async () => {
+        const response = await axios.get(`${API}/categories`);
+        setState({ ...state, categories: response.data });
+    };
+
+    const listCategories = () =>
+        categories.map((c, i) => (
+            <Link href={`/links/${c.slug}`}>
+                <a style={{ border: '1px solid red' }} className="bg-light p-3 col-md-6">
+                    <div>
+                        <div className="row">
+                            <div className="col-md-4">
+                                <img
+                                    src={c.image && c.image.url}
+                                    alt={c.name}
+                                    style={{ width: '100px', height: 'auto' }}
+                                    className="pr-3"
+                                />
+                            </div>
+                            <div className="col-md-8">
+                                <h3>{c.name}</h3>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </Link>
+        ));
