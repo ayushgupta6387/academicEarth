4a104b63f0ff95b9049d8b9b1868fd78da089dc6
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-12 16:27:49 +0530

    category read Imlementation

---
+++ client/pages/admin/category/read.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API } from '../../../config';
+import Link from 'next/link';
+import { showSuccessMessage, showErrorMessage } from '../../../helpers/alerts';
+import Layout from '../../../components/Layout';
+import withAdmin from '../../withAdmin';
+
+const Read = ({ user, token }) => {
+    const [state, setState] = useState({
+        error: '',
+        success: '',
+        categories: []
+    });
+
+    const { error, success, categories } = state;
+
+    useEffect(() => {
+        loadCategories();
+    }, []);
