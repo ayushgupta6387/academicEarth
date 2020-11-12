ed180935dc4bf5cd9c0948b0d2123b4fa88b98e3
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-12 16:26:43 +0530

    Category read

--- client/pages/admin/index.js
+++ client/pages/admin/index.js
@@ -1,26 +1,29 @@
 import Layout from '../../components/Layout';
 import withAdmin from '../withAdmin';
-import Link from 'next/Link'
-const Admin = ({ user }) => <Layout>
-    {/* {JSON.stringify(user)} */}
-    <h1>Admin Dashboard</h1>
-    <br/>
-    <div className='row'>
-        <div className='col-md-4'>
-            <ul className='nav flex-column'>
-                <li className='nav-item'>
-                        <Link href="/admin/category/create">
-                            <a className='nav-link ' style={{color:'white',background:'orange',borderRadius:'10px',textAlign:'center'}}>Create Category</a>
-                        </Link>
-                </li>
-            </ul>
+import Link from 'next/link';
 
+const Admin = ({ user }) => (
+    <Layout>
+        <h1>Admin Dashboard</h1>
+        <br />
+        <div className="row">
+            <div className="col-md-4">
+                <ul className="nav flex-column">
+                    <li className="nav-item">
+                        <Link href="/admin/category/create">
+                            <a className="nav-link">Create category</a>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/admin/category/read">
+                            <a className="nav-link">All categories</a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="col-md-8"></div>
         </div>
-        <div className='col-md-8'></div>
-    </div>
-    
-    
-    
-    </Layout>;
+    </Layout>
+);
 
 export default withAdmin(Admin);
