5973c9eb3fa31de57d3e84f90f132bae06a7cbd9
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-30 13:24:59 +0530

    IAM Enabled

--- client/config.js
+++ client/config.js
@@ -4,3 +4,5 @@ const { publicRuntimeConfig } = getConfig();
 export const API = publicRuntimeConfig.API;
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 export const DOMAIN = publicRuntimeConfig.DOMAIN;
+export const PRODUCTION = publicRuntimeConfig.PRODUCTION;
+export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
